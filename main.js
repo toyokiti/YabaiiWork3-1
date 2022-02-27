3507a73d781f84c83d5182b975b9fdd19f18b211
toyokiti <akiakiakihiro0525@icloud.com>
2022-02-27 16:06:40 +0900

    3回目

--- main.js
+++ main.js
@@ -2,10 +2,7 @@
 {
     const tasks = []; //入力したタスクを格納する配列
     const inputTask = (name, status = '作業中') => {
-        /*
-        tasks配列の末尾に、タスクオブジェクトをプッシュする
-        tasks配列を返す。
-        */
+        // tasks配列の末尾に、タスクオブジェクトをプッシュする
         const newtask = {
             id:  tasks.length,
             name:  name,
@@ -14,83 +11,69 @@
         tasks.push(newtask);    
     }
 
-    // HTML要素に、テキスト＆クラスネームを追加する。関数
-    const custumTdElement = (element, text, className) => {
-        element.textContent = text;
+    // HTML要素に、クラス名＆テキストを追加する。関数
+    const addClassAndText = (element, text, className) => {
         element.classList.add(className);
+        element.textContent = text;
     }
 
     // テーブルに記載のタスクをすべて削除する関数 
-    const deleteTaskTable = (element) => {
-        while(element.firstChild) {
-            element.removeChild(element.firstChild);
-        }
+    const deleteTaskTable = () => {
+        document.getElementById('tbody').textContent='';
     }
 
-    // 対象がテーブルの何行目かを確認する関数
-    const serchTargetRow = (id) => {
-        const tr = document.getElementById('tbody').getElementsByTagName('tr');
-        let result
-        Array.prototype.forEach.call(tr, (value, index)=> {
-            if (id === Number(value.firstChild.textContent)) {
-                result = index;
-            }
-        })
-        return result;
-    }
-    
     // ステータスボタンを生成する関数
-    const createStatusBtn = (task) => {
-        const btn = document.createElement('button');
-        custumTdElement(btn, task.status, 'statusBtn');
-        const tr = document.getElementById('tbody').getElementsByTagName('tr');
-        tr[serchTargetRow(task.id)].getElementsByClassName('status')[0].appendChild(btn);
+    const createStatusBtn = (task, tr) => {
+        // ボタン作成
+        const button = document.createElement('button');
+        addClassAndText(button, task.status, 'statusButton');
+        // td取得＆ボタン追加
+        const td = tr.getElementsByClassName('status')[0]
+        td.appendChild(button);
+        //todo: 課題3-3でここに、ステータスボタンをクリックすると作業中⇔完了とするイベント追加
     }
 
     // 削除ボタンを生成する関数
-    const createDeleteBtn = (task) => {
-        const btn = document.createElement('button');
-        custumTdElement(btn, '削除', 'deleteBtn');
-        const tr = document.getElementById('tbody').getElementsByTagName('tr');
-        tr[serchTargetRow(task.id)].getElementsByClassName('status')[0].appendChild(btn);
-
-        // TODO: 課題3-2では、ここに削除ボタンをクリックしたら、タスクが消えるイベントを追加
+    const createDeleteBtn = (tr) => {
+        // ボタン作成
+        const button = document.createElement('button');
+        addClassAndText(button, '削除', 'deleteButton');
+        // td取得＆ボタン追加
+        const td = tr.getElementsByClassName('status')[0]
+        td.appendChild(button);
+        //todo: 課題3-2でここに、削除ボタンをクリックするとタスクを削除するイベント追加
     }
 
     // ブラウザに表示されているタスクを一旦削除してから再描画する関数
     const refleshTable = () => {
-
-        const taskTable = document.getElementById('tbody');
-        deleteTaskTable(taskTable);
+        // HTMLのtbodyの中身を空にして、ブラウザ上のタスクを消す。
+        const tbody = document.getElementById('tbody');
+        deleteTaskTable(tbody);
 
         tasks.forEach((value) => {
+            // trの作成
             const tr = document.createElement('tr');
-            const ID = document.createElement('td');
+            // tdの作成
+            const id = document.createElement('td');
             const name = document.createElement('td');
             const status = document.createElement('td');
-            
             // 各HTML要素に、テキストとクラス名を追加
-            custumTdElement(ID, value.id, 'id');
-            custumTdElement(name, value.name, 'taskName' );
-            custumTdElement(status, '', 'status');
-            // テーブルに<tr>を追加
-            taskTable.appendChild(tr);
-            // 追加した<tr>の中に<td>を追加
-            const lastTableRow = taskTable.lastElementChild;
-            lastTableRow.appendChild(ID);
-            lastTableRow.appendChild(name);
-            lastTableRow.appendChild(status);
-            
-            // 状態列にボタンを追加
-            createStatusBtn(value);
-            createDeleteBtn(value);
+            addClassAndText(id, value.id, 'id');
+            addClassAndText(name, value.name, 'taskName' );
+            addClassAndText(status, '', 'status');
+            // trの中にtdを追加
+            tr.appendChild(id);
+            tr.appendChild(name);
+            tr.appendChild(status);
+            // 状態列にボタンをそれぞれ追加
+            createStatusBtn(value, tr);
+            createDeleteBtn(tr);
+            // テーブルにtrを追加
+            tbody.appendChild(tr);
         });
     }
 
-    /**
-    * 新しいタスクを追加する関数
-    * tasksに、新しいタスクを追加して表示する 
-    */ 
+    // 新しいタスクを追加する関数: tasksに、新しいタスクを追加して表示する 
     const addTask = (taskName) => {
         const tasks = inputTask(taskName);
         refleshTable();
